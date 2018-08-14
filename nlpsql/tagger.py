105d936f00059e5ad63c46851abc3f91794de01f
MiloRuan <ruanzihua2005@163.com>
2018-08-14 15:15:49 +0800

    update the isAdj function

--- nlpsql/tagger.py
+++ nlpsql/tagger.py
@@ -13,6 +13,7 @@ def backoff_tagger(train_sents, tagger_classes, backoff=None):
 
 
 class SQLPosTagger(SequentialBackoffTagger):
+
     def isEntity(self, word):
         entities = ['DW_F_JOURNAL','DW_F_JOURNAL.BUSINESS_DATE','DW_F_JOURNAL.CURRENCY','DW_F_JOURNAL.FTP_ALN','DW_F_JOURNAL.ID_ENTITY','DW_F_JOURNAL.SITE_CODE','DW_F_LEDGER_BALANCE','DW_F_LEDGER_BALANCE.ALN','DW_F_LEDGER_BALANCE.BUSINESS_DATE','DW_F_LEDGER_BALANCE.CCY','DW_F_LEDGER_BALANCE.ID_ENTITY','DW_F_LEDGER_BALANCE.SITE_CODE','DW_H_BUSINESS_NATURE','DW_H_BUSINESS_NATURE.','FINANCIAL_ELEMENT']
         if word in entities:
@@ -29,22 +30,23 @@ class SQLPosTagger(SequentialBackoffTagger):
             return False
 
 
-    def isAdv(self, word):
+    def isAdj(self, word, next_word):
         advList = ['right', 'left', 'inner', 'outter', 'full']
-        if word.lower() in advList:
+        sqlKeyWord  = ['join']
+        if word.lower() in advList and next_word in sqlKeyWord:
             return True
         else:
             return False
 
     def choose_tag(self, tokens, index, history):
+        size = len(tokens)
         if index == 0 and SQLPosTagger.isVerb(self, tokens[index]):
             return "VB"
-        if SQLPosTagger.isAdv(self, tokens[index]) and tokens[index + 1] == "join" :
+        if index + 1 < size and SQLPosTagger.isAdj(self, tokens[index], tokens[index + 1]):
             return "ADJ"
         if SQLPosTagger.isEntity(self, tokens[index]):
             return "NN"
         return None
-
 
 def myParse(sentence):
     print("ClassifierBasedPOSTagger tag:")
@@ -65,6 +67,6 @@ def defaultParse(sentence):
 
 if __name__ == '__main__':
     #sentence = "Search the data from database"
-    sentence = "DW_F_JOURNAL outter join DW_F_JOURNAL)"
+    sentence = "Search all data in DW_F_JOURNAL left join DW_F_JOURNAL"
     myParse(sentence)
     defaultParse(sentence)
